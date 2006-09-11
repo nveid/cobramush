fc454dcb3d0d24e2b0f24a34b93b4761d7d5df09
nveid <nveid@comcast.net>
2006-09-11 05:16:07 +0000

    mysql hints - makes it so mysql is detected in certain servers such as
    fedora and generally where mysql is installed in a subdirectory in the
    libpath

--- hints/linux_2.sh
+++ hints/linux_2.sh
@@ -1,2 +1,14 @@
 nm_opt='-B'
+
+# sometimes mysql might be found in an actual mysql
+# subdirectory in the library path.. We'll need to
+# check for that
+
+if [ -d /usr/lib/mysql ]; then
+   ldflags='-L/usr/lib/mysql'
+ elif [ -d /usr/local/lib/mysql ]; then
+    ldflags='-L/usr/local/lib/mysql'
+fi
+
+
 echo "I suggest using sysmalloc when you edit options.h."
