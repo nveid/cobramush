60ea53f20d657bc802ae66aa05128d34c535b4bc
Ari Johnson <ari@cobramush.org>
2007-02-20 17:18:48 +0000

    Combined and simplified FreeBSD hints file

--- hints/freebsd.sh
+++ hints/freebsd.sh
@@ -1,6 +1,6 @@
 usenm=false
 i_malloc='undef'
 i_values='undef'
-ccflags='-I/usr/local/include'
+ccflags='-D_POSIX_C_SOURCE=2 -D__XSI_VISIBLE=1000 -D__BSD_VISIBLE -I/usr/local/include'
 d_attribut=true
-
+d_force_ipv4='define'
--- hints/freebsd_5.sh
+++
@@ -1,6 +0,0 @@
-usenm=false
-i_malloc='undef'
-i_values='undef'
-ccflags='-D_POSIX_C_SOURCE=2 -D__XSI_VISIBLE=1000 -D__BSD_VISIBLE -I/usr/local/include'
-d_attribut=true
-d_force_ipv4='define'
