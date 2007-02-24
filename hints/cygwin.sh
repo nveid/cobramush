b5409589e7f99f7143fdda28228a68f68c64b3ca
Ari Johnson <ari@cobramush.org>
2007-02-24 17:01:18 +0000

    Removed hints/cygwin.sh

--- hints/cygwin.sh
+++
@@ -1,7 +0,0 @@
-# Hints for cygwin, tested with 1.3.12-2
-# Note that you need the exim-4.10-1 package so you can include
-# its minires files
-cc='gcc'
-ccflags='-I/usr/src/exim-4.10-1/minires'
-usenm='y'
-d_ipv6='undef'
