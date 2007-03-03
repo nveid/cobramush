2ace728e0e49725b94f66089fa75d06109822aec
Ari Johnson <ari@cobramush.org>
2007-03-03 04:08:53 +0000

    Made preparedist.sh set scripts executable (cherry picked from commit d411810fb99c29be1e89f65be40177f4a9e96c9c)

--- utils/preparedist.sh
+++ utils/preparedist.sh
@@ -1,19 +1,30 @@
 #!/bin/sh
 
+# Bootstrap
+chmod 755 "$0"
+
 SELF=`which "$0"`
 DIR=`dirname "$SELF"`
 cd "$DIR"
 cd ..
 
-# Step 1: run metaconfig
+# Step 1: set appropriate scripts executable
+echo "Setting scripts executable..."
+chmod 755 utils/customize.pl
+chmod 755 utils/make_access_cnf.sh
+chmod 755 utils/mkvershlp.pl
+chmod 755 utils/update-cnf.pl
+chmod 755 utils/update.pl
+
+# Step 2: run metaconfig
 echo "Running metaconfig to generate Configure and config_h.SH..."
 metaconfig -M
 
-# Step 2: generate changes.txt
+# Step 3: generate changes.txt
 echo "Generating changes.txt..."
 ( cd game/txt && ruby genchanges.rb )
 
-# Step 3: index help files
+# Step 4: index help files
 echo "Indexing help, news, and events..."
 cd game/txt
 for i in hlp nws evt ; do
