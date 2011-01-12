3a603fdd914569e3d22e7e83672a83827bb18919
Ari Johnson <ari@theari.com>
2011-01-12 20:37:11 +0000

    Change order of events in preparedist.sh

--- utils/preparedist.sh
+++ utils/preparedist.sh
@@ -16,15 +16,26 @@ chmod 755 utils/make_access_cnf.sh
 chmod 755 utils/update-cnf.pl
 chmod 755 utils/update.pl
 
-# Step 2: run metaconfig
+# Step 2: create auto-generated source/header files
+echo "Generating source and header files..."
+cd src
+swig -o mushlua_wrap.c -lua mushlua.i
+cd ..
+
+perl utils/mkcmds.pl all
+mv hdrs/cmds.h win32/
+mv hdrs/switches.h win32/
+mv hdrs/funs.h win32/
+
+# Step 3: run metaconfig
 echo "Running metaconfig to generate Configure and config_h.SH..."
 metaconfig -M
 
-# Step 3: generate changes.txt
+# Step 4: generate changes.txt
 echo "Generating changes.txt..."
 ( cd game/txt && ruby genchanges.rb )
 
-# Step 4: index help files
+# Step 5: index help files
 echo "Indexing help, news, and events..."
 cd game/txt
 for i in hlp nws evt ; do
@@ -35,15 +46,4 @@ for i in hlp nws evt ; do
 done
 
 cd ../..
-
-# Step 5: create auto-generated source/header files
-echo "Generating source and header files..."
-cd src
-swig -o mushlua_wrap.c -lua mushlua.i
-cd ..
-
-perl utils/mkcmds.pl all
-mv hdrs/cmds.h win32/
-mv hdrs/switches.h win32/
-mv hdrs/funs.h win32/
 
